66f86f7668c444bb952ae32e4eeb16a41bf9d034
Jon <Jon@192.168.1.24>
2016-04-03 15:10:42 -0400

    Added try-catch to dns_server.java and created makefile.

--- UDP Socket Tutorial/src/udp/dns_server.java
+++ UDP Socket Tutorial/src/udp/dns_server.java
@@ -15,24 +15,24 @@ import java.util.Arrays;
 
 
 public class dns_server{ static //it doesn't want me to name it dns-server
-	
+
 	int responseSize;
-	
-	public static void main(String[] args)throws Exception{
-	
-		
-		//Command names + Command line processing
-			/*
-			 * FORMAT
-			 * (1) accept a port number with -p parameter
-			 * (2) accept hostfile with -f parameter
-			 */
-		
-		int port = 0; //port numberwi
-		
-		//HostFile need to insert bw that reads from plain txt and stores input
-		
-				
+
+public static void main(String[] args)throws Exception{
+
+
+	//Command names + Command line processing
+	/*
+	 * FORMAT
+	 * (1) accept a port number with -p parameter
+	 * (2) accept hostfile with -f parameter
+	 */
+
+	int port = 0; //port numberwi
+
+	//HostFile need to insert bw that reads from plain txt and stores input
+
+	try {		
 		if(args.length == 0) // error checking, can't be blank
 		{
 			port = 12345;
@@ -58,306 +58,170 @@ public class dns_server{ static //it doesn't want me to name it dns-server
 			}
 		} else {
 			System.out.println("You're stupid. Do it right.");
-			System.exit(0);
+			System.exit(1);
 		}
-		
-		
-	//Simple server that receives a UDP packet on a port
-		DatagramSocket serverSocket = new DatagramSocket(port);
-		System.out.println(serverSocket.getPort());
-		String Website;
-		
-			byte[] recieveData = new byte[1024]; 
-			byte[] sendData = new byte[1024];
-			while(true){
-				DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length);
-				System.out.println("Waiting for packet.");
-				serverSocket.receive(recievePacket);
-				sendData = examine(recievePacket.getData(), recievePacket.getLength());
-				System.out.println(recievePacket.getData().length);
-				System.out.println("Packet received.");
-				String sentence = new String(Arrays.copyOfRange(recieveData, 13, 24), StandardCharsets.UTF_8);
-				//System.out.println("Recieved: " + sentence); 
-				//Website = Arrays.copyOfRange(recieveData, 12, 16);
-				//System.out.println("The website is: " + Website.toString());
-				InetAddress IPAddress = recievePacket.getAddress();
-				int port1 = recievePacket.getPort();
-				
-				DatagramPacket sendPacket = new DatagramPacket(sendData, responseSize);
-				sendPacket.setPort(port1);
-				sendPacket.setAddress(IPAddress);
-				serverSocket.send(sendPacket);
-				 
-			}
-	//Parse the DNS message
-		//Parse the DNS header
-			//Error check for network vs host byte order
-		// Parse out Query string
-	
-			
-	//Create a response
-			
-	//Process the Hosts file
-			
+	} catch(NumberFormatException nfe) {
+		System.out.println("You're stupid. Do it right.");
+		System.exit(1);
 	}
-	
 
-	static byte[] examine(byte[] pbuf, int plen) {
 
-		// dump what we have for debugging
-		// a line of hex first, then a line of characters (where possible)
+	//Simple server that receives a UDP packet on a port
+	DatagramSocket serverSocket = new DatagramSocket(port);
+	System.out.println(serverSocket.getPort());
+	String Website;
 
-		System.out.println("Received: " + plen + " bytes: ");
-		for (int i=0; i < plen; i++)
+	byte[] recieveData = new byte[1024]; 
+	byte[] sendData = new byte[1024];
+	while(true){
+		DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length);
+		System.out.println("Waiting for packet.");
+		serverSocket.receive(recievePacket);
+		sendData = examine(recievePacket.getData(), recievePacket.getLength());
+		System.out.println(recievePacket.getData().length);
+		System.out.println("Packet received.");
+		String sentence = new String(Arrays.copyOfRange(recieveData, 13, 24), StandardCharsets.UTF_8);
+		//System.out.println("Recieved: " + sentence); 
+		//Website = Arrays.copyOfRange(recieveData, 12, 16);
+		//System.out.println("The website is: " + Website.toString());
+		InetAddress IPAddress = recievePacket.getAddress();
+		int port1 = recievePacket.getPort();
+
+		DatagramPacket sendPacket = new DatagramPacket(sendData, responseSize);
+		sendPacket.setPort(port1);
+		sendPacket.setAddress(IPAddress);
+		serverSocket.send(sendPacket);
+
+	}
+	//Parse the DNS message
+	//Parse the DNS header
+	//Error check for network vs host byte order
+	// Parse out Query string
+
+
+	//Create a response
+
+	//Process the Hosts file
+
+}
+
+
+static byte[] examine(byte[] pbuf, int plen) {
+
+	// dump what we have for debugging
+	// a line of hex first, then a line of characters (where possible)
+
+	System.out.println("Received: " + plen + " bytes: ");
+	for (int i=0; i < plen; i++)
+		System.out.print(String.format("%02x ", pbuf[i]));
+	System.out.println("");
+	for (int i=0; i < plen; i++)
+		if ((pbuf[i] <= ' ') || (pbuf[i] > '~'))
 			System.out.print(String.format("%02x ", pbuf[i]));
-		System.out.println("");
-		for (int i=0; i < plen; i++)
-			if ((pbuf[i] <= ' ') || (pbuf[i] > '~'))
-				System.out.print(String.format("%02x ", pbuf[i]));
-			else
-				System.out.print(String.format("%c  ", pbuf[i]));
-		System.out.println("");
+		else
+			System.out.print(String.format("%c  ", pbuf[i]));
+	System.out.println("");
 
-		// look at the bytes as big endian shorts
-		// the wrap() method uses an existing byte array for the buffer
+	// look at the bytes as big endian shorts
+	// the wrap() method uses an existing byte array for the buffer
 
-		short[] shorts = new short[plen/2];
-		ByteBuffer.wrap(pbuf).order(ByteOrder.BIG_ENDIAN).asShortBuffer().get(shorts);
+	short[] shorts = new short[plen/2];
+	ByteBuffer.wrap(pbuf).order(ByteOrder.BIG_ENDIAN).asShortBuffer().get(shorts);
 
-		// dump our buffer as shorts
-		for (int i=0; i < plen/2; i++)
-			System.out.println("short[" + i + "] = " + shorts[i]);
+	// dump our buffer as shorts
+	for (int i=0; i < plen/2; i++)
+		System.out.println("short[" + i + "] = " + shorts[i]);
 
-		// another way we can create shorts is by manually putting 2 bytes together
-		// internet format is big endian - the first byte has the more significant value
-		// this one produces an unsigned result
+	// another way we can create shorts is by manually putting 2 bytes together
+	// internet format is big endian - the first byte has the more significant value
+	// this one produces an unsigned result
 
-		int short_value = ((pbuf[0] & 0xff) << 8) + (pbuf[1] & 0xff);
-		System.out.println("first 16 bits = " + short_value);
+	int short_value = ((pbuf[0] & 0xff) << 8) + (pbuf[1] & 0xff);
+	System.out.println("first 16 bits = " + short_value);
 
-		System.out.println();
-		// demo of extracting bit fields (e.g., for dns)
-		// grab the second group of two bytes and treat it as a 16 bit set of bits
-		// bits are indexed left to right
-		int v = (pbuf[2] & 0xff) << 8 | (pbuf[3] & 0xff);
-		
-		//Query String begins on index 12 (Index 12 is the byte count, whereas 13 is the start of the String)
-		int size = Integer.parseInt(String.format("%d" , pbuf[12]));
-		int start = 13;
-		int end;
-		
-		
-		System.out.print(String.format("%d ", pbuf[13]));
-		int check = 0;
-		String domainName = "";
-		
-		do{
-			
-			if(check == 1){
-				domainName = domainName.concat(".");
-				System.out.print(".");
-			} else {
-				check = 1;
-			}
-		
+	System.out.println();
+	// demo of extracting bit fields (e.g., for dns)
+	// grab the second group of two bytes and treat it as a 16 bit set of bits
+	// bits are indexed left to right
+	int v = (pbuf[2] & 0xff) << 8 | (pbuf[3] & 0xff);
+
+	//Query String begins on index 12 (Index 12 is the byte count, whereas 13 is the start of the String)
+	int size = Integer.parseInt(String.format("%d" , pbuf[12]));
+	int start = 13;
+	int end;
+
+
+	System.out.print(String.format("%d ", pbuf[13]));
+	int check = 0;
+	String domainName = "";
+
+	do{
+
+		if(check == 1){
+			domainName = domainName.concat(".");
+			System.out.print(".");
+		} else {
+			check = 1;
+		}
+
 		for (int i = 0; i < size; i++){
 			domainName = domainName.concat(String.format("%c", pbuf[start]));
 			System.out.print(String.format("%c", pbuf[start]));
 			start++;
 		}
-		
-		
+
+
 		size = Integer.parseInt(String.format("%d", pbuf[start]));
 		end = start++;
-		
-		} while(!((pbuf[start] <= ' ') || (pbuf[start] > '~'))); //do a comparison to 0? maybe later.
-		System.out.println();
-		
-		//Question Type
-		
-		
-		int QType = ((pbuf[start] & 0xff) << 8) + (pbuf[start+1] & 0xff);
-		
-		for(int j=0; j <16; j++){
-			if(j != 15){
-				QType &= ~(1<<(15-j));
-			} else {
-				QType |= (1<<(15-j));
-			}
+
+	} while(!((pbuf[start] <= ' ') || (pbuf[start] > '~'))); //do a comparison to 0? maybe later.
+	System.out.println();
+
+	//Question Type
+
+
+	int QType = ((pbuf[start] & 0xff) << 8) + (pbuf[start+1] & 0xff);
+
+	for(int j=0; j <16; j++){
+		if(j != 15){
+			QType &= ~(1<<(15-j));
+		} else {
+			QType |= (1<<(15-j));
 		}
-		
-		pbuf[start] = (byte) ((QType >> 8) & 0xff);
-		pbuf[start+1] = (byte) (QType & 0xff);
-		
-		int QClass = ((pbuf[start+2] & 0xff) << 8) + (pbuf[start+3] & 0xff);
-		
-		for(int j=0; j <16; j++){
-			if(j != 15){
-				QClass &= ~(1<<(15-j));
-			} else {
-				QClass |= (1<<(15-j));
-			}
+	}
+
+	pbuf[start] = (byte) ((QType >> 8) & 0xff);
+	pbuf[start+1] = (byte) (QType & 0xff);
+
+	int QClass = ((pbuf[start+2] & 0xff) << 8) + (pbuf[start+3] & 0xff);
+
+	for(int j=0; j <16; j++){
+		if(j != 15){
+			QClass &= ~(1<<(15-j));
+		} else {
+			QClass |= (1<<(15-j));
 		}
-		
-		pbuf[start+2] = (byte) ((QClass >> 8) & 0xff);
-		pbuf[start+3] = (byte) (QClass & 0xff);
-		System.out.println("");
-	
-		responseSize = start + 4;
-			
-		System.out.println("The domain name you are looking for is: " + domainName);
-		
-		File f = new File("hosts.txt");
-		//String[] domains = parser.parse(f);
-		Domain[] domains  = parser.parse(f);
-		boolean domainExists = false;
-		
-		for(int i = 0; i < domains.length; i++){
-			String ip = domains[i].getAddress();
-			String name = domains[i].getHost();
+	}
 
-			if(domainName.equalsIgnoreCase(name)){
-				domainExists = true;
-				//set qr to 1
-				v |= (1<<(15-0));
-				//set aa to 1
-				v |= (1<<(15-5));
-				//set tc to 0
-				v &= ~(1<<(15-6));
-				//set ra to 0
-				v &= ~(1<<(15-8));
-				//set rd to 0
-				v &= ~(1<<(15-7));
-				
-				//Set rcode to 3 for error (address not found)
-				v &= ~(1<<(15-12)); 
-				v &= ~(1<<(15-13));
-				v &= ~(1<<(15-14));
-				v &= ~(1<<(15-15));
-				
-				pbuf[2] = (byte) ((v >> 8) & 0xff);
-				pbuf[3] = (byte) (v & 0xff);
-				
-				int ANCount = ((pbuf[6] & 0xff) << 8) + (pbuf[7] & 0xff);
-				System.out.println("ANCount 16 bits = " + ANCount);
-				
-				for(int j=0; j <16; j++){
-					if(j != 15){
-						ANCount &= ~(1<<(15-j));
-					} else {
-						ANCount |= (1<<(15-j));
-					}
-				}
-				
-				pbuf[6] = (byte) ((ANCount >> 8) & 0xff);
-				pbuf[7] = (byte) (ANCount & 0xff);
-				
-				System.out.println("The ip address: " + ip);
-				
-				
+	pbuf[start+2] = (byte) ((QClass >> 8) & 0xff);
+	pbuf[start+3] = (byte) (QClass & 0xff);
+	System.out.println("");
 
-				int l = 0;
-							
-				//offset
-				for(int j = 12; j < end+1; j++){
-					pbuf[start+4+l] = pbuf[j];
-					l++;
-				}
-				
-				int curr = start + 4 + l;
+	responseSize = start + 4;
 
-				
-				int Type = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
-				
-				for(int j=0; j <16; j++){
-					if(j != 15){
-						Type &= ~(1<<(15-j));
-					} else {
-						Type |= (1<<(15-j));
-					}
-				}
-				
-				pbuf[curr] = (byte) ((Type >> 8) & 0xff);
-				pbuf[curr+1] = (byte) (Type & 0xff);
-				
-				curr+=2;
-				
-				int classy = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
-				
-				for(int j=0; j <16; j++){
-					if(j != 15){
-						classy &= ~(1<<(15-j));
-					} else {
-						classy |= (1<<(15-j));
-					}
-				}
-				
-				pbuf[curr] = (byte) ((classy >> 8) & 0xff);
-				pbuf[curr+1] = (byte) (classy & 0xff);
-				
-				curr+=4;
-				
-				int ttl = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
-				
-				for(int j=0; j <16; j++){
-						ttl &= ~(1<<(15-j));
-				}
-				
-				pbuf[curr] = (byte) ((ttl >> 8) & 0xff);
-				pbuf[curr+1] = (byte) (ttl & 0xff);
-				
-				curr+=2;
-				
-				int rdlength = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
-				
-				for(int j=0; j <16; j++){
-					if(j != 13){
-						rdlength &= ~(1<<(15-j));
-					} else {
-						rdlength |= (1<<(15-j));
-					}
-				}
-				
-				pbuf[curr] = (byte) ((rdlength >> 8) & 0xff);
-				pbuf[curr+1] = (byte) (rdlength & 0xff);
-				
-				int one = Integer.parseInt(ip.substring(0, ip.indexOf(".")));
-				ip = ip.substring(ip.indexOf(".") + 1);
-				int two = Integer.parseInt(ip.substring(0, ip.indexOf(".")));
-				ip = ip.substring(ip.indexOf(".") + 1);
-				int three = Integer.parseInt(ip.substring(0, ip.indexOf(".")));
-				ip = ip.substring(ip.indexOf(".") + 1);
-				int four = Integer.parseInt(ip);
-				
-				System.out.println(one + "." + two + "." + three+ "." + four);
-				
-				curr+=2;
+	System.out.println("The domain name you are looking for is: " + domainName);
 
-				
-				pbuf[curr] = (byte) one;
-				pbuf[curr+1] = (byte) two;
-				pbuf[curr+2] = (byte) three;
-				pbuf[curr+3] = (byte) four;
-				responseSize = curr+4;
-				
-				
-				for (int j=0; j <= 47; j++)
-						System.out.print(String.format("%02x ", pbuf[j]));
-				System.out.println("");
-				
-				for (int j=0; j <= 47; j++)
-					if ((pbuf[j] <= ' ') || (pbuf[j] > '~')){
-						System.out.print(String.format("%02x ", pbuf[j]));}
-					else{
-						System.out.print(String.format("%c  ", pbuf[j]));}
-				System.out.println("");
-				
-				break;
-			} 
-		}
-		
-		if(domainExists == false) {
-			
+	File f = new File("hosts.txt");
+	//String[] domains = parser.parse(f);
+	Domain[] domains  = parser.parse(f);
+	boolean domainExists = false;
+
+	for(int i = 0; i < domains.length; i++){
+		String ip = domains[i].getAddress();
+		String name = domains[i].getHost();
+
+		if(domainName.equalsIgnoreCase(name)){
+			domainExists = true;
 			//set qr to 1
 			v |= (1<<(15-0));
 			//set aa to 1
@@ -368,38 +232,178 @@ public class dns_server{ static //it doesn't want me to name it dns-server
 			v &= ~(1<<(15-8));
 			//set rd to 0
 			v &= ~(1<<(15-7));
-			
+
 			//Set rcode to 3 for error (address not found)
 			v &= ~(1<<(15-12)); 
 			v &= ~(1<<(15-13));
-			v |= (1<<(15-14));
-			v |= (1<<(15-15));
-			
-			for (int x=0; x < 16; x++) {
-				System.out.println("bit[" + x + "] = " + (v>>(15-x) & 1));
-				// System.out.println("bit[" + i + "] = " + (v & 1<<(15-i)));
-			}
-			
+			v &= ~(1<<(15-14));
+			v &= ~(1<<(15-15));
+
 			pbuf[2] = (byte) ((v >> 8) & 0xff);
 			pbuf[3] = (byte) (v & 0xff);
-	
-		//set ANCount to 0
-			
+
 			int ANCount = ((pbuf[6] & 0xff) << 8) + (pbuf[7] & 0xff);
 			System.out.println("ANCount 16 bits = " + ANCount);
-			
+
 			for(int j=0; j <16; j++){
+				if(j != 15){
 					ANCount &= ~(1<<(15-j));
+				} else {
+					ANCount |= (1<<(15-j));
+				}
 			}
-			
+
 			pbuf[6] = (byte) ((ANCount >> 8) & 0xff);
 			pbuf[7] = (byte) (ANCount & 0xff);
-			
-		}
-		
-		return pbuf;
-		
+
+			System.out.println("The ip address: " + ip);
+
+
+
+			int l = 0;
+
+			//offset
+			for(int j = 12; j < end+1; j++){
+				pbuf[start+4+l] = pbuf[j];
+				l++;
+			}
+
+			int curr = start + 4 + l;
+
+
+			int Type = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
+
+			for(int j=0; j <16; j++){
+				if(j != 15){
+					Type &= ~(1<<(15-j));
+				} else {
+					Type |= (1<<(15-j));
+				}
+			}
+
+			pbuf[curr] = (byte) ((Type >> 8) & 0xff);
+			pbuf[curr+1] = (byte) (Type & 0xff);
+
+			curr+=2;
+
+			int classy = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
+
+			for(int j=0; j <16; j++){
+				if(j != 15){
+					classy &= ~(1<<(15-j));
+				} else {
+					classy |= (1<<(15-j));
+				}
+			}
+
+			pbuf[curr] = (byte) ((classy >> 8) & 0xff);
+			pbuf[curr+1] = (byte) (classy & 0xff);
+
+			curr+=4;
+
+			int ttl = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
+
+			for(int j=0; j <16; j++){
+				ttl &= ~(1<<(15-j));
+			}
+
+			pbuf[curr] = (byte) ((ttl >> 8) & 0xff);
+			pbuf[curr+1] = (byte) (ttl & 0xff);
+
+			curr+=2;
+
+			int rdlength = ((pbuf[curr] & 0xff) << 8) + (pbuf[curr+1] & 0xff);
+
+			for(int j=0; j <16; j++){
+				if(j != 13){
+					rdlength &= ~(1<<(15-j));
+				} else {
+					rdlength |= (1<<(15-j));
+				}
+			}
+
+			pbuf[curr] = (byte) ((rdlength >> 8) & 0xff);
+			pbuf[curr+1] = (byte) (rdlength & 0xff);
+
+			int one = Integer.parseInt(ip.substring(0, ip.indexOf(".")));
+			ip = ip.substring(ip.indexOf(".") + 1);
+			int two = Integer.parseInt(ip.substring(0, ip.indexOf(".")));
+			ip = ip.substring(ip.indexOf(".") + 1);
+			int three = Integer.parseInt(ip.substring(0, ip.indexOf(".")));
+			ip = ip.substring(ip.indexOf(".") + 1);
+			int four = Integer.parseInt(ip);
+
+			System.out.println(one + "." + two + "." + three+ "." + four);
+
+			curr+=2;
+
+
+			pbuf[curr] = (byte) one;
+			pbuf[curr+1] = (byte) two;
+			pbuf[curr+2] = (byte) three;
+			pbuf[curr+3] = (byte) four;
+			responseSize = curr+4;
+
+
+			for (int j=0; j <= 47; j++)
+				System.out.print(String.format("%02x ", pbuf[j]));
+			System.out.println("");
+
+			for (int j=0; j <= 47; j++)
+				if ((pbuf[j] <= ' ') || (pbuf[j] > '~')){
+					System.out.print(String.format("%02x ", pbuf[j]));}
+				else{
+					System.out.print(String.format("%c  ", pbuf[j]));}
+			System.out.println("");
+
+			break;
+		} 
 	}
-	
-	
+
+	if(domainExists == false) {
+
+		//set qr to 1
+		v |= (1<<(15-0));
+		//set aa to 1
+		v |= (1<<(15-5));
+		//set tc to 0
+		v &= ~(1<<(15-6));
+		//set ra to 0
+		v &= ~(1<<(15-8));
+		//set rd to 0
+		v &= ~(1<<(15-7));
+
+		//Set rcode to 3 for error (address not found)
+		v &= ~(1<<(15-12)); 
+		v &= ~(1<<(15-13));
+		v |= (1<<(15-14));
+		v |= (1<<(15-15));
+
+		for (int x=0; x < 16; x++) {
+			System.out.println("bit[" + x + "] = " + (v>>(15-x) & 1));
+			// System.out.println("bit[" + i + "] = " + (v & 1<<(15-i)));
+		}
+
+		pbuf[2] = (byte) ((v >> 8) & 0xff);
+		pbuf[3] = (byte) (v & 0xff);
+
+		//set ANCount to 0
+
+		int ANCount = ((pbuf[6] & 0xff) << 8) + (pbuf[7] & 0xff);
+		System.out.println("ANCount 16 bits = " + ANCount);
+
+		for(int j=0; j <16; j++){
+			ANCount &= ~(1<<(15-j));
+		}
+
+		pbuf[6] = (byte) ((ANCount >> 8) & 0xff);
+		pbuf[7] = (byte) (ANCount & 0xff);
+
+	}
+
+	return pbuf;
+
+}
+
+
 }
